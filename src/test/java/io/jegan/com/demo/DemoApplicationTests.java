d3c97d5ca689390b38f2770304193a661324e5b0
Jeganath.Rengasamy <Jeganath2020gmail.com>
2022-04-19 12:51:24 -0400

    gateway lms

---
+++ src/test/java/io/jegan/com/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jegan.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
